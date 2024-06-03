cafd0c6808c145c78a223628102bc04ae909f6e6
Sam Puttman <daputter2@gmail.com>
2024-06-03 11:56:37 -0500

    Update plant_generation_service to properly set the content type to json

--- app/services/plant_generation_service.rb
+++ app/services/plant_generation_service.rb
@@ -1,10 +1,13 @@
 class PlantGenerationService
   def self.generate_plant(name, description, plant_type)
-    response = Faraday.post('https://be-plant-microservce-b4648dce0143.herokuapp.com/generate_plant', {
-      name: name,
-      description: description,
-      plant_type: plant_type
-    }.to_json, 'Content-Type' => 'application/json')
+    response = Faraday.post('http://localhost:3001/generate_plant') do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = {
+        name: name,
+        description: description,
+        plant_type: plant_type
+      }.to_json
+    end
 
     JSON.parse(response.body)
   end
