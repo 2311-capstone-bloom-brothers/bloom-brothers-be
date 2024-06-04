2b41d59c68b46227b81fd1b9665e044086c8c350
Joey Reyes <joeyreyes105@gmail.com>
2024-06-04 11:07:04 -0500

    Update serivce to plant genrate to take arguement position, update test and vcr

--- app/services/plant_generation_service.rb
+++ app/services/plant_generation_service.rb
@@ -1,11 +1,12 @@
 class PlantGenerationService
-  def self.generate_plant(name, description, plant_type)
+  def self.generate_plant(name, description, plant_type, position)
     response = Faraday.post('https://be-plant-microservce-2db5d13ea731.herokuapp.com/generate_plant') do |req|
       req.headers['Content-Type'] = 'application/json'
       req.body = {
         name: name,
         description: description,
-        plant_type: plant_type
+        plant_type: plant_type,
+        position: position
       }.to_json
     end
 
