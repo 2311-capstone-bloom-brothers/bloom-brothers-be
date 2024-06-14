5b4496bec178a024d1aa571a1cbaea3653474526
Sam Puttman <daputter2@gmail.com>
2024-06-14 14:22:13 -0500

    Update to utilize locally hosted generator

--- app/services/plant_generation_service.rb
+++ app/services/plant_generation_service.rb
@@ -1,6 +1,7 @@
 class PlantGenerationService
   def self.generate_plant(name, description, plant_type, position)
-    response = Faraday.post('https://be-plant-microservce-2db5d13ea731.herokuapp.com/generate_plant') do |req|
+    # response = Faraday.post('https://be-plant-microservce-2db5d13ea731.herokuapp.com/generate_plant') do |req|
+    response = Faraday.post('http://localhost:3001/generate_plant') do |req|
       req.headers['Content-Type'] = 'application/json'
       req.body = {
         name: name,
